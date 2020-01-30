73fac695ac15f2f8477fcc3e44af4bb613ddff22
Gynir Starvin <gstarvin@at-j3.21-school.ru>
2020-01-30 13:51:40 +0300

    a

--- gsprintf/main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-
-int main() {
-//      printf("%hd\n", 555);
-      ft_printf("%hhd\n", 555);
-//    printf("orig =\n%+010.5d|\n", 555);
-//      ft_printf("my f =\n%+010.5d|\n", 555);
-    return 0;
-}
